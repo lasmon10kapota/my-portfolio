bde009a105ba269124b18da0a9832f2364ec906f
lasmon10kapota <133053264+lasmon10kapota@users.noreply.github.com>
2023-06-17 20:04:09 +0200

    Update databaseconnection.php

--- databaseconnection.php
+++ databaseconnection.php
@@ -7,6 +7,7 @@ $databasename="my_portfolio";
 $conn=mysqli_connect($servername, $username, $password) or die("No Connection!");
 mysqli_select_db($conn, $databasename) or die("Database could not open!");
 
+//Creating a function that will be retrieving data from database in the  skills table
 function mySkills(){
     global $conn;
     $query = "SELECT * FROM skills";
@@ -22,7 +23,7 @@ function mySkills(){
     }
 }
 
-
+//Creating a function that will be retrieving data from database in the  projects table
 function myProjects(){
     global $conn;
     $query = "SELECT * FROM projects";
@@ -37,6 +38,21 @@ function myProjects(){
         $i++;
     }
 }
+//for the contact form in the index.php to check if the details are set
+// and then post them to the database in contact table
+if(isset($_POST['submit'])){
+    $name = $_POST['fname'];
+    $email = $_POST['emel'];
+    $address = $_POST['adress'];
+    $message = $_POST['message'];
+    $query = "INSERT INTO contact(name, email, address, message) VALUES ('$name', '$email', '$address' '$message')";
+    $sql = mysqli_query($conn, $query);
+    if($sql){
+        header('location: index.php');
+    } else {
+        echo "Query failed" . mysqli_error($conn);
+    }
 
+}
 
 ?>
