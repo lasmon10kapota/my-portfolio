f9fbf06d9345a57ebdf0c7f39d95a244c7bbafcd
lasmon10kapota <133053264+lasmon10kapota@users.noreply.github.com>
2023-08-29 20:06:12 +0200

    Add files via upload
    I made some correction to css files

---
+++ aboutme.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Lasmon's Portfolio</title>
+        <link rel="stylesheet" href="nav.css">
+        <script src="myjavascript.js"></script>
+        <style>
+          
+            .container{
+              display:flex;
+              align-items: center;
+              column-gap: 30px;
+            }
+            img{
+              margin-left: 50px;
+              width: 500px;
+              height: 500px;
+              border: 5px solid #2e3550;
+              border-radius: 20px;
+            }
+
+            h3{
+              color:#2e3550;
+              margin-right: 50px;
+              line-height: 1.8;
+            }
+
+            p{
+              margin-left: 20px;
+              margin-right: 20px;
+              font-family: Arial;
+              font-size: 20px;
+            }
+            .para, address{
+              text-align: center;
+              line-height: 1.6;
+            }
+
+        </style>    
+    </head>
+
+    <body onload="onLoadingPage()">
+        <header>
+            <nav>
+                <ul>
+                    <h2 title="The owner of this Website, ICT Expert.">LASMON KAPOTA</h2>
+                    <li><a  href="Projects.php">Projects</a></li>
+                    <li><a  href="skills.php">Skills</a></li>
+                    <li><a  href="contact.php">Contact</a></li>
+                    <li class="active"><a  href="aboutme.php">About Me</a></li>
+                    <li><a  href="index.php">Home</a></li>
+                  </ul>
+            </nav>
+            
+        </header>
+
+        <div class="container">
+            <div class="img">
+              <img src="MyPicture.jpg" alt="My PhotoShop picture that I first created."/>
+            </div>
+            <div>
+            <h3 style="color:#2e3550">
+              Having observed the constantly advancing technology, it is my pleasure to work in the field
+              of Infomation and communication technology (ICT) which is now the only dominating field across the world.
+              It has been always my dream to pursue this awesome aspect of science and thank God for making my 
+              belated dream come true. I made it from secondary education with so satisfactory grade that set a bridge 
+              for me to cross to tertially education to chase the course of my desire, ICT. As I speak am still on 
+              my way chasing pages at Mzuzu University in Malawi and anytime soon I am going to graduate to be ICT degree holder then. However, 
+              I would like to say with gladness that I have got competent skills so far in this course that I can do
+              so many wonderful things like Web designing and development, building different mobile and desktop applications,
+              graphic designing, video production, and many more. My vision is to uplift technological life by providing
+              people from different parts of the world with more sophisticated information technology that will make life easier and enjoyable.
+            </h3>
+          </div>
+        </div>
+        <br><br>
+
+        <h1 style="text-align: center;" style="color: #2e3550;">Other Relevant Infomation</h1>
+        <p>
+          The information provided below indicates some of the things that I am part of and I think may be helpful for other people know 
+          for various reasons that they can have to communicate with me:
+        </p>
+        <h2 class="para">
+          My Educational background
+        </h2>
+        <p class="para">
+          Currently learning at Mzuzu University<br>University in northen region of Malawi<br>Pursuing bachelor of scieance in Information and Communication Technology (ICT)<br>
+          Currently in secong Year<br>Got various <a href="skills.php">skills</a> including software development and manipulation
+          <address>
+            Mzuzu University<br>
+            P/Bag 201<br>
+            Luwinga, Mzuzu<br>
+            Registrar's email address : ur@mzuni.ac.mw<br>
+            Website <a href="https://www.mzuni.ac.mw">www.mzuni.ac.mw</a>
+          </address>
+        </p>
+        <h2 class="para">
+          My Personal particulars
+        </h2>
+        <p class="para">
+          Lasmon Kapota<br>Male<br>Aged 22 <br>Unmarried<br>Malawian<br>Having Both parents<br>Christian of SDA (Seventh Day Adventist) Church<br>
+          More contact information, <a href="contact.php">click here</a>
+        </p>
+      
+        <footer>
+            <h2 title="Email: lsmnkapota@gmail.com & Tel: 0998306445"> &copy; Lasmon ICT Viewpoint</h2>
+        </footer>
+
+    </body>
+    
+</html>
